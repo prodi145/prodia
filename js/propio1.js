eec0020b9946aa4a5b21330daf0582cf59335e77
prodi145 <anggz1236@gmail.com>
2020-06-28 18:37:21 -0500

    prodia

---
+++ js/propio1.js
@@ -0,0 +1,7 @@
+var mensaje =   "Bienvenido";
+var mensaje2= "ESTA ES MI WEB PERSONAL"
+var mensaje3 = "PRODIANGELO TE DESEA UN GRANDIOSO DIA";
+
+alert(mensaje)
+alert(mensaje2)
+alert(mensaje3)        
